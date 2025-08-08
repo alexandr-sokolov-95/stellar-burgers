5f35f78425549e95bf1a0c5500f576de5bcdc5e1
Alexandr Sokolov <alexandrsokolov.dev@gmail.com>
2025-08-08 13:23:20 +0300

    Test edits 3

--- cypress/e2e/construtor-page.cy.ts
+++ cypress/e2e/construtor-page.cy.ts
@@ -1,4 +1,7 @@
-import { setCookie } from '../../src/utils/cookie';
+const ingredientSelector = '[data-testid="ingredient"]';
+const constructorSelector = '[data-testid="constructor-element"]';
+const modalSelector = '[data-testid="modal"]';
+const modalCloseSelector = '[data-testid="modal-close"]';
 
 describe('проверяем доступность приложения', function () {
   beforeEach(() => {
@@ -21,7 +24,7 @@ describe('проверяем доступность приложения', funct
     localStorage.setItem('refreshToken', 'test-access-token');
     cy.setCookie('accessToken', 'test-access-token');
 
-    cy.visit('http://localhost:4000');
+    cy.visit('/');
   });
 
   this.afterAll(() => {
@@ -30,13 +33,13 @@ describe('проверяем доступность приложения', funct
   });
 
   it('должны загружаться ингридиенты', () => {
-    cy.get('[data-testid="ingredient"]').should('have.length', 4);
+    cy.get(ingredientSelector).should('have.length', 4);
   });
 
   it('должны добавляться ингридиенты в конструктор', () => {
-    cy.get('[data-testid="constructor-element"]').as('constructor');
+    cy.get(constructorSelector).as('constructor');
 
-    cy.get('[data-testid="ingredient"]').each((el, index) => {
+    cy.get(ingredientSelector).each((el, index) => {
       if (index !== 2) {
         cy.wrap(el).within(() => {
           cy.get('button').contains('Добавить').should('exist').as('add');
@@ -60,7 +63,7 @@ describe('проверяем доступность приложения', funct
 
     cy.location('pathname').should('match', /\/ingredients\/.+/);
 
-    cy.get('[data-testid="modal"]').as('modal');
+    cy.get(modalSelector).as('modal');
 
     cy.get('@modal').within(() => {
       cy.contains('Детали ингридиента').should('exist');
@@ -68,9 +71,7 @@ describe('проверяем доступность приложения', funct
     });
 
     cy.get('@modal').within(() => {
-      cy.get('[data-testid="modal-close"]')
-        .as('modal-close-butoon')
-        .should('exist');
+      cy.get(modalCloseSelector).as('modal-close-butoon').should('exist');
 
       cy.get('@modal-close-butoon').click();
     });
@@ -90,9 +91,9 @@ describe('проверяем доступность приложения', funct
   });
 
   it('должен создаться и отправиться заказ', () => {
-    cy.get('[data-testid="constructor-element"]').as('constructor');
+    cy.get(constructorSelector).as('constructor');
 
-    cy.get('[data-testid="ingredient"]').each((el, index) => {
+    cy.get(ingredientSelector).each((el, index) => {
       cy.wrap(el).within(() => {
         cy.get('button').contains('Добавить').should('exist').as('add');
 
@@ -105,23 +106,35 @@ describe('проверяем доступность приложения', funct
       cy.get('@order').click();
     });
 
-    cy.wait('@orderBurger').then(() => {
-      cy.get('[data-testid="modal"]').should('exist').as('modal');
-      cy.get('@modal').contains('2222');
-
-      cy.get('[data-testid="modal-close"]').click();
-
-      cy.get('@modal').should('not.exist');
-
-      cy.get('@constructor').within(() => {
-        cy.get('[data-testid="constructor-filled-element"]').should(
-          'not.exist'
-        );
-        cy.get('[data-testid="constructor-empty-element"]').should(
-          'have.length',
-          3
-        );
+    cy.wait('@orderBurger')
+      .its('request.body')
+      .should('deep.equal', {
+        ingredients: ['1', '2', '3', '4']
       });
+
+    cy.get('@orderBurger')
+      .its('response.body')
+      .should('deep.equal', {
+        success: true,
+        name: 'Тестовый бургер',
+        order: {
+          number: 2222
+        }
+      });
+
+    cy.get(modalSelector).should('exist').as('modal');
+    cy.get('@modal').contains('2222');
+
+    cy.get(modalCloseSelector).click();
+
+    cy.get('@modal').should('not.exist');
+
+    cy.get('@constructor').within(() => {
+      cy.get('[data-testid="constructor-filled-element"]').should('not.exist');
+      cy.get('[data-testid="constructor-empty-element"]').should(
+        'have.length',
+        3
+      );
     });
   });
 });
